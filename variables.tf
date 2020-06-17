cafe46af965695b96dbe606173c3a9e46163f386
vladc <vlad@aucsys.co.uk>
2020-06-17 09:28:33 +0100

    initial

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "state_machine_name" {
+  type = string
+}
+
+variable "state_machine_definition" {
+  type = string
+}
+
+variable "state_machine_vars" {
+  type = map(string)
+}
+
+variable "controlled_functions" {
+  description = "arns of lambda functions participating in this state machine"
+  type        = list(string)
+}
